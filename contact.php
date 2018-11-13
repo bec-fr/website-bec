4f485c4ca0cfc8ea7653508fcf0024fb9f1d8e2f
Saliou LO <zalesalioulo@gmail.com>
2018-11-13 10:54:02 +0000

    Changement formulaire contact

--- contact.php
+++ contact.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<? include("connect.php"); ?>
+ <? include("connect.php"); ?>
 <html lang="fr">
     <head>        
-        <title>Bec séjours linguistiques - Nous contacter</title>  
+        <title>Bec sï¿½jours linguistiques - Nous contacter</title>  
         
         <meta name="description" content="Comment contacter le BEC">
-        <meta name="author" content="BEC Séjours linguistiques"> 
+        <meta name="author" content="BEC Sï¿½jours linguistiques"> 
         
         <!-- Mobile Metas -->
         <meta name="viewport" content="width=device-width,  minimum-scale=1,  maximum-scale=1">
@@ -68,33 +68,7 @@
                                   <h1>Nous adresser un message</h1>
 						</div>
 						
-                                  
-                                  <?
-            if(isset($_POST["ok"]) && $_POST["ok"] == 1){
-	//$mail_site = "contact@becfrance.com";
-	if(envoi_mail($mail_site, "Message depuis votre site Internet ".$url_site2, "Vous avez re&ccedil;u un message depuis votre site Internet ".$url_site2."<br><br>
-
-Nom : ".htmlentities($_POST["nom"])."<br>
-Pr&eacute;nom : ".htmlentities($_POST["prenom"])."<br>
-Adresse : ".htmlentities($_POST["adresse"]." ".$_POST["cp"]." ".$_POST["ville"])."<br>
-T&eacute;l : ".htmlentities($_POST["tel"])."<br>
-Recherche : ".htmlentities($_POST["rechercher"])."<br>
-Mail : ".htmlentities($_POST["mail"])."<br><br>
-
-Message : ".nl2br(htmlentities($_POST["message"]))."<br>
-
-")){
-		echo "<br><br><div align='center' class='texteBleu'><b>Merci, votre demande a bien été envoyée !</b></div><br><br>";
-		
-		if(trim($_POST["mail"]) != ""){
-			mysql_query("INSERT INTO mailing (mail) VALUES ('".addslashes($_POST["mail"])."')");
-		}
-	}else{
-		echo "<br><br><div align='center' class='texteBleu'><b>Votre demande n'a pas pu être envoyée.</b></div><br><br>";
-	}
-}else{
-
-?>                                  
+                                        
                 
 				 <div class="row"> 
 						<address>
@@ -116,52 +90,56 @@ Message : ".nl2br(htmlentities($_POST["message"]))."<br>
 				</div>		
 				
 				<br>
-                  <form class="form-horizontal" action="" method="post" name="mail" id="mail">
-                    <input type="hidden" value="1" name="ok" />
+                  <form action="http://localhost/becfrance-site/posted-contact.php" method="POST" name="WebToLeadForm"  class="form-horizontal">
+					<div class="form-group">
+								<label  class="col-sm-3 control-label">Titre* : </label>
+								<div class="col-sm-9">
+									<select id="salutation" name="salutation" class="select">
+										<option selected>---</option>
+										<option value="M.">M.</option>
+										<option value="Mme">Mme</option>
+									</select>
+								</div>
+						</div>
+						<div class="form-group">
+								<label  class="col-sm-3 control-label">Nom* : </label>
+								<div class="col-sm-9"><input required name="last_name" type="text" id="last_name" value=""  size="40" /></div>
+						</div>	
+						<div class="form-group">
+								<label  class="col-sm-3 control-label">Pr&eacute;nom* : </label>
+								<div class="col-sm-9"><input required name="first_name" type="text" id="first_name" value=""  size="40" /></div>
+						</div>
 					
-					<div class="form-group">
-							<label  class="col-sm-3 control-label">Nom* : </label>
-							<div class="col-sm-9"><input required name="nom" type="text" id="nom" value=""  size="40" /></div>
-					</div>	
-					<div class="form-group">
-							<label  class="col-sm-3 control-label">Pr&eacute;nom* : </label>
-							<div class="col-sm-9"><input required name="prenom" type="text" id="prenom" value="" " size="40" /></div>
-					</div>
-                   <div class="form-group">
-							<label  class="col-sm-3 control-label">Adresse : </label>
-							<div class="col-sm-9"><input name="adresse" type="text" id="adresse" value=""  size="40" /></div>
-					</div>
-					 <div class="form-group">
-							<label  class="col-sm-3 control-label">Code postal </label>
-							<div class="col-sm-9"><input name="cp" type="text" id="cp" value="" size="8" /></div>
-					</div>
-					 <div class="form-group">
-							<label  class="col-sm-3 control-label">Ville :</label>
-							<div class="col-sm-9"><input name="ville" type="text" id="ville" value=""  size="40" /></div>
-					</div>
-					 <div class="form-group">
-							<label  class="col-sm-3 control-label">T&eacute;l : </label>
-							<div class="col-sm-9"><input name="tel" type="text" id="tel" value=""  size="10" /></div>
-					</div>
-					 <div class="form-group">
-							<label  class="col-sm-3 control-label">Mail*  </label>
-							<div class="col-sm-9"><input required name="mail" type="email" id="mail" value=""  size="40" /></div>
-					</div>
-					 <div class="form-group">
-							<label  class="col-sm-3 control-label">Vous recherchez :</label>
-							<div class="col-sm-9"><select name="rechercher" class="select"><option value="">- - -</option><option value="un séjour junior">un séjour junior</option><option value="un séjour étudiant/adulte">un séjour étudiant/adulte</option><option value="un voyage scolaire">un voyage scolaire</option></select></div>
-					</div>
-					 <div class="form-group">
-                   		<label  class="col-sm-3 control-label">Message :</label>
-							<div class="col-sm-9"><textarea name="message" cols="50" rows="8" id="message" class="textarea"></textarea></div>
-					</div>
-               
-                   	<div align="right" class="col-sm-9" ><input  align="right" type="submit"  onClick="javascript:verifForm()" class="button" value="Adresser votre message"></div>                
-                 
-                   
+						<div class="form-group">
+								<label  class="col-sm-3 control-label">T&eacute;l : </label>
+								<div class="col-sm-9"><input name="phone_mobile" type="text" id="phone_mobile" value=""  size="10" /></div>
+						</div>
+						<div class="form-group">
+								<label  class="col-sm-3 control-label">Mail*  </label>
+								<div class="col-sm-9"><input required name="email1" type="email" id="email1" value=""  size="40" /></div>
+						</div>
+						<div class="form-group">
+								<label  class="col-sm-3 control-label">Vous recherchez :</label>
+								<div class="col-sm-9">
+									<select name="segment" class="select">
+										<option value="">- - -</option>
+										<option value="un sï¿½jour junior">un sï¿½jour junior</option>
+										<option value="un sï¿½jour ï¿½tudiant/adulte">un sï¿½jour ï¿½tudiant/adulte</option>
+										<option value="un voyage scolaire">un voyage scolaire</option>
+									</select>
+								</div>
+						</div>
+						<div class="form-group">
+							<label  class="col-sm-3 control-label">Message :</label>
+								<div class="col-sm-9"><textarea name="message" cols="50" rows="8" id="message" class="textarea"></textarea></div>
+						</div>
+					
+						<div align="right" class="col-sm-9" >
+						<input  align="right" type="submit"   class="button" value="Adresser votre message">
+						</div>
+                    
                   </form>
-               
-                <? } ?>
+            
 				</div>
                                   
                                  <!-- fin contenu -->
